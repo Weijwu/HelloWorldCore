7e7313c11d9e4ee7c71dd0cdf57bbc9c9b0112cb
weijie <weijie.wu@virtuedx.com>
2021-11-01 09:58:11 +0800

    update

---
+++ HelloWorldCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace HelloWorldCore
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello World!");
+    //    }
+    //}
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
