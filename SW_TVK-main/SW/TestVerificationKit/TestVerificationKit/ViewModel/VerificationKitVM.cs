a6564e01778eeedbb8008dd99e03a91bd9351f42
weijie <weijie.wu@virtuedx.com>
2021-11-01 14:02:57 +0800

    change project

---
+++ SW_TVK-main/SW/TestVerificationKit/TestVerificationKit/ViewModel/VerificationKitVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtue.iGene_Pad.TVK.ViewModel
+{
+    public partial class VerificationKitVM: ViewModelBase
+    {
+
+
+    }
+}
